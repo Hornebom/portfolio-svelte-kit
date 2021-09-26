9d8b01d6e834a5a1df586d62e5079617b3f67a87
Benedikt Deitmer <you@hornebom.com>
2021-09-26 11:44:01 +0200

    - deletes all starter components and css - implements dato request load function - set one slug route - adds netlify adapter

---
+++ src/lib/footer/Footer.svelte
@@ -0,0 +1,20 @@
+<script>
+	import { page } from '$app/stores'
+
+	export let pages
+</script>
+
+<footer>
+  <ul>
+    {#each pages as { slug, title }}
+      <li class:active={$page.path === `/${slug}`}>
+        <a sveltekit:prefetch href={`/${slug}`}>
+          {title}
+        </a>
+      </li>
+    {/each}
+  </ul>
+</footer>
+
+<style>
+</style>
---
+++ src/routes/__error.svelte
@@ -0,0 +1,19 @@
+<script context="module">
+  export function load({ error, status }) {
+		return {
+			props: {
+				title: `${status}: ${error.message}`
+			}
+		}
+	}
+</script>
+
+<script>
+	export let title
+</script>
+
+<h1>{title}</h1>
+<a sveltekit:prefetch href="/">Home</a>
+
+<style>
+</style>
