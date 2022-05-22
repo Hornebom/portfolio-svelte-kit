b32403255ccdac4ed4e4f2ec3294d1e0f52ca558
Benedikt Deitmer <you@hornebom.com>
2022-05-22 13:02:50 +0200

    removes comments

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,14 +1,6 @@
 <script context="module">
-	// import query from './layout.query.js'
-	// import datoRequest from '$lib/utils/dato-request.js'
 	import loadData from '../lib/utils/loadData.js'
 
-	// export async function load({ page, fetch }) {
-	// 	const token = import.meta.env.VITE_DATO_API_TOKEN
-	// 	const { navigation, allSocials } = await datoRequest({ query, fetch, token })
-
-	// 	return { props: { navigation, socials: allSocials } }
-	// }
 	export const load = async({ fetch }) => loadData(fetch, 'data/app.json')
 </script>
 
